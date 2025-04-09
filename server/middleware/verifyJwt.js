f67ad3bff7c0b73a11dd9b40fa8cb03b3de250ec
Dey <Day4y3@proton.me>
2025-04-09 11:32:01 +0200

    stuff happened

--- server/middleware/verifyJwt.js
+++ server/middleware/verifyJwt.js
@@ -2,13 +2,10 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
 async function verifyJwt(req, res, next) {
-    console.log('Cookies received:', req.cookies);
-    console.log('JWT cookie:', req.cookies.jwt);
     
     const token = req.cookies.jwt;
 
     if (!token) {
-        console.log('No token provided');
         return res.status(401).json({ message: 'Unauthorized: No token' });
     }
 
@@ -21,18 +18,12 @@ async function verifyJwt(req, res, next) {
         user = await User.findOne({ email: decoded.email });
 
         if (!user) {
-            console.log('User not found:', decoded.email);
             return res.status(401).json({ message: 'Unauthorized: User not found' });
         }
 
         req.user.id = user._id;
         next();
     } catch (error) {
-        console.log('Error verifying token or finding user:', error);
-        console.log('Token:', token);
-        console.log('Decoded:', decoded);
-        console.log('User:', user);
-
         return res.status(401).json({ message: 'Unauthorized: Invalid token or user' });
     }
 }
